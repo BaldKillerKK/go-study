a623391a026bb9c90937ba35bf117d478e60c173
81087033 <tianbo1@yonghui.cn>
2024-02-06 23:19:37 +0800

    feat: add user handler signup func

--- webook/internal/main.go
+++ webook/internal/main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+	"go-study/webook/internal/web"
+)
+
 func main() {
 
+	server := gin.Default()
+	u := &web.UserHandler{}
+
+	u.RegisterUserRoutes(server)
+
+	server.Run(":8080")
 }
--- webook/internal/web/user.go
+++ webook/internal/web/user.go
@@ -1,5 +1,49 @@
 package web
 
+import "github.com/gin-gonic/gin"
+
 // UserHandler 定义和用户有关的路由
 type UserHandler struct {
+}
+
+// RegisterUserRoutes 用户路由注册
+func (u *UserHandler) RegisterUserRoutes(server *gin.Engine) {
+	// 路由分组
+	ug := server.Group("/users")
+	ug.GET("/profile", u.Profile)
+	ug.POST("/login", u.Login)
+	ug.POST("/signup", u.Signup)
+	ug.POST("/edit", u.Edit)
+}
+
+// Signup 用户注册
+func (u *UserHandler) Signup(ctx *gin.Context) {
+	type signupReq struct {
+		Email           string `json:"email"`
+		Password        string `json:"password"`
+		ConfirmPassword string `json:"confirmPassword"`
+	}
+
+	var req signupReq
+
+	if err := ctx.Bind(&req); err != nil {
+		return
+	}
+
+	ctx.String(200, "signup %v", req)
+}
+
+// Login 用户登录
+func (u *UserHandler) Login(ctx *gin.Context) {
+
+}
+
+// Edit 用户修改
+func (u *UserHandler) Edit(ctx *gin.Context) {
+
+}
+
+// Profile 用户查看
+func (u *UserHandler) Profile(ctx *gin.Context) {
+
 }
